7817e148679dda6297c94e85eab687f1233f1bc4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 15:22:00 +0100

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 import './main.scss';
 
-import AddTodo from './components/addTodo/addTodo';
-import TodoList from './components/todoList/todoList';
-import TodoItem from './components/todoList/todoItem/todoItem';
+require('./components/addTodo/addTodo');
+require('./components/todoList/todoList');
+require('./components/todoList/todoItem/todoItem');
